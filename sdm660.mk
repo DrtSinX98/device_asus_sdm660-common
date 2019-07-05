e7864a742cbf3c026f8ec41e6c36e6ac626dd146
Rizky Benggolo <travarilo@gmail.com>
2019-07-05 18:12:51 +0000

    [SQUASHED] sdm660-common: Import ZenfoneParts v2
    Mostly imported from https://github.com/xiaomi-sdm660/android_device_xiaomi_sdm660-common
    Gesture stuff's taken from https://github.com/SagarMakhar/device_asus_X00TD
    
    All credit's belong to them
    
    * overlays added
    * node permissions added
    Danish1999 -
    * Add permissions to separate rc file
    * Remove Zenmotion
    Co-authored-by: Fikri <Genkzsz11@gmail.com>
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>
    Change-Id: I4f943138f6a3e7b421669253073cec187440f8bf

--- sdm660.mk
+++ sdm660.mk
@@ -272,8 +272,8 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc \
-    init.safailnet.rc
-
+    init.safailnet.rc \
+    init.zenparts.rc
 
 # IPv6
 PRODUCT_PACKAGES += \
@@ -492,6 +492,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+# ZenParts
+PRODUCT_PACKAGES += \
+    ZenParts
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/asus/sdm660-common/sdm660-common-vendor.mk)
